2523266c5cc68e870cb17ea53e5f171bcf966a5d
Sarla Bharath Chandra <sarlabharathchandra@gmail.com>
2026-04-22 14:38:40 +0530

    configured database url

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -17,9 +17,13 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
+
+
+
+#def get_db():
+  #  db = SessionLocal()
+   # try:
+    #    yield db
+    #finally:
+     #   db.close()
